1d3e826bb2af391282cc586f6abd5e092813d8fa
Luis Gomes <lmagomes@pm.me>
2024-01-09 12:03:15 +0000

    Small changes and formating

--- provisioning/terraform/providers.tf
+++ provisioning/terraform/providers.tf
@@ -3,35 +3,35 @@ terraform {
 
   required_providers {
     proxmox = {
-        source = "Telmate/proxmox"
-        version = "2.9.13"
+      source  = "Telmate/proxmox"
+      version = "2.9.13"
     }
   }
 }
 
 variable "proxmox_api_url" {
-    type = string
+  type = string
 }
 
 variable "proxmox_api_token_id" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "proxmox_api_token_secret" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 provider "proxmox" {
-    # pm_log_enable = true
-    # pm_log_file   = "terraform-plugin-proxmox.log"
-    # pm_debug      = true
-    # pm_log_levels = {
-    #     _default    = "debug"
-    #     _capturelog = ""
-    # }
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
-    pm_api_token_secret = var.proxmox_api_token_secret
+  # pm_log_enable = true
+  # pm_log_file   = "terraform-plugin-proxmox.log"
+  # pm_debug      = true
+  # pm_log_levels = {
+  #     _default    = "debug"
+  #     _capturelog = ""
+  # }
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
 }
